f182d6e718de9f83f9399206fa82a225be798d96
Mukul Khandelwal <mukulkhandelwal1997@gmail.com>
2017-02-08 21:11:58 +0530

    remaining error solved

--- linearregression.py
+++ linearregression.py
@@ -9,18 +9,19 @@ def compute_error_for_line_given_points(b,m,points):
 	#for every point
 
 	for i in range(0,len(points)):
+		
 		#get the x value
-		x = points(i,0)
+		x = points[i,0]
 
 		#get the y value
-		y = points(i,1)
+		y = points[i,1]
 
 		#get the differene ,square it and add it to the total
 		totalError += (y - (m * x + b)) ** 2
 
 		#get the average
 
-		return totalError/float(len(points))
+	return totalError/float(len(points))
 
 
 
@@ -51,22 +52,24 @@ def step_gradient(b_current,m_current, points , learningRate):
 	N = float(len(points))
 	for i in range(0,len(points)):
 
-		x=points(i,0)
-		y=points(i,1)
+		x=points[i,0]
+		y=points[i,1]
 
 		#direction with respect to b and m
 		#computing partial derivatives of our error function
 
-		b_gradient += -(2/N) * (x * (y - ( m_gradient*x + b_gradient)))
+		b_gradient += -(2/N) * (x * (y - ( m_current*x + b_current)))
 
 		m_gradient += -(2/N) * x * (y - ((m_current*x) + b_current ))
 
+	     # b_gradient += -(2/N) * (y - ((m_current * x) + b_current))
+      #   m_gradient += -(2/N) * x * (y - ((m_current * x) + b_current))
 
 		#update our b and m values using our partial derivatives
 
-		new_b = b_current - (learningRate * b_gradient)
-		new_m = m_current - (learningRate * m_gradient)
-
+	new_b = b_current - (learningRate * b_gradient)
+	new_m = m_current - (learningRate * m_gradient)
+	return [new_b, new_m]
 
 
 
@@ -90,14 +93,14 @@ def run():
 
 	#step3 - train our model
 
-	print("starting gradient descent at  b ={0},m={1},error={2}").format(initial_b,initial_m,compute_error_for_line_given_points(initial_b , initial_m , points))
+	print("starting gradient descent at  b ={0},m={1},error={2}".format(initial_b,initial_m,compute_error_for_line_given_points(initial_b , initial_m , points)))
 	print ("Running...")
 
 	[b,m] = gradient_descent_runner(points,initial_b,initial_m,learning_rate,num_iteration)
 
 
-	print("ending gradient descent at  b = {1} , m = {2} , error = {3}").format(num_iteration,b,m,compute_error_for_line_given_points(b,m,points))
+	print("ending gradient descent at  b = {1} , m = {2} , error = {3}".format(num_iteration,b,m,compute_error_for_line_given_points(b,m,points)))
 
 
-if __main__ =='__main__':
+if __name__ == '__main__' :
 	run()
